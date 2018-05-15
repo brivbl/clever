7adb464b01179f9b80a78b4f00dc09aba8d9987f
Brieuc <b4vably@enib.fr>
2018-05-15 14:41:34 +0200

    first commit

---
+++ server_express.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+var http = require('http').Server(app);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+var beers=[];
+
+
+
+app.get('/beers', function (req, res) {
+	res.status(200);
+  res.send(JSON.stringify(beers));
+});
+
+app.get('/beer', function (req, res) {
+	res.status(200);
+  res.send(JSON.stringify(beers[req.id]));
+});
+
+app.delete('/beer', function(req, res){
+		res.status(200);
+		beers.splice(req.body.name,1);
+		res.send(beers);
+});
+
+app.post('/beer', function(req, res){
+	var beer = {
+		name : req.body.name
+	};
+	beers.push(beer);
+	res.status(200);
+	res.send(beers);
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
